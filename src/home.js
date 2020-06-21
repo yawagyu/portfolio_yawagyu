cb51f8ea74b5c8975acbcc40a21f87c8cc35ad46
yawagyu <yawagyu@gmail.com>
2020-06-21 18:48:35 +0200

    Created-app

---
+++ src/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Webdesigner & Front-end Developer</h2>
+
+        <p>I design websites and code them in a beautifull way</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
