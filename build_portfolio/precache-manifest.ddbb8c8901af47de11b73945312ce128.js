8243211d4d63f4c3c00ace530e64b200ca9e3b8a
yawagyu <yawagyu@gmail.com>
2020-07-13 15:23:25 +0200

    updated footer + build

---
+++ build_portfolio/precache-manifest.ddbb8c8901af47de11b73945312ce128.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896bb2284f56b65199b3f819fc692ca8",
+    "url": "/build_portfolio/index.html"
+  },
+  {
+    "revision": "17b05f8adbca6dcdc71d",
+    "url": "/build_portfolio/static/css/main.cf0c8af0.chunk.css"
+  },
+  {
+    "revision": "d949de1392081b57620c",
+    "url": "/build_portfolio/static/js/2.344995e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/build_portfolio/static/js/2.344995e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b05f8adbca6dcdc71d",
+    "url": "/build_portfolio/static/js/main.57b71bcb.chunk.js"
+  },
+  {
+    "revision": "7ee354ed7b09f4d6ad33",
+    "url": "/build_portfolio/static/js/runtime-main.2905c4c2.js"
+  },
+  {
+    "revision": "2a56eeb0712ee7432ab0cb869399a94d",
+    "url": "/build_portfolio/static/media/forest_illustration_mobile_transparent.2a56eeb0.png"
+  },
+  {
+    "revision": "528ff5311bcd257760bf213866337097",
+    "url": "/build_portfolio/static/media/forest_illustration_transparent.528ff531.png"
+  }
+]);
